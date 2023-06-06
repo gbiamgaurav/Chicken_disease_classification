705e932a5cb59dbcb0692581240e83d899e2f740
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-06-06 11:25:09 +0530

    data_ingestion stage completed

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from chicken_disease import logger
+from chicken_disease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our Custom Log")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} Started <<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> Stage {STAGE_NAME} completed <<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e 
